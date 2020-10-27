59936480fb55e7300070d30bbdc62f946493c2fd
caprioara <dragos.neatax@gmail.com>
2020-10-27 11:53:37 +0200

    11 - Handling Dynamic Routing

--- tweets/views.py
+++ tweets/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import Tweet
 
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Hello Dragos</h1>")
+    return HttpResponse("<h1>Home view</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content} </h1>")
